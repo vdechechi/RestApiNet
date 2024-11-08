2fcdac3c9cb8ac27e8eb2553d02fa204f0c708c7
Vinicius <vinicius.gdechechi@gmail.com>
2024-11-07 22:01:22 -0300

    Models Criadas

---
+++ Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace RESTAPI.Models
+{
+    public class Comment
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+
+        //Navigation property
+        public Stock? Stock{ get; set; }
+    }
+}
