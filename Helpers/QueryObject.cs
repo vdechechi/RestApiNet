b227eb327b9d039fe8f45b9d86a51e2557221070
Vinicius <vinicius.gdechechi@gmail.com>
2024-11-09 15:59:31 -0300

    Paginação

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -6,5 +6,8 @@
         public string? CompanyName { get; set; } = String.Empty;
         public string? SortBy { get; set; } = null;
         public bool IsDescending { get; set; } = false;
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 20;
     }
 }
--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -66,7 +66,9 @@ namespace RESTAPI.Repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
